7fe53f8482f668a922c50b59ad1aab094ae8f35a
Alexandra Bozzini <alexandrabozzini@zipcodersmbp10.lan>
2019-02-09 20:48:34 -0500

    Added help message

--- Display.java
+++ Display.java
@@ -12,7 +12,19 @@ public class Display
     }
     
     public static void displayHelp() {
-        String helpMsg = "This is the help message";
+        String helpMsg = "Format "
+        + "\ninput double, operator, and another double(if needed)"
+        +"\nThe following require two doubles to be entered:"
+        + "\n+ => addition"
+        + "\n- => subtraction"
+        + "\n* => multiplication"
+        + "\n/ => division"
+        + "\npow => power"
+        + "\nThe following only require one double to be entered:"
+        + "\nsq => square"
+        + "\nsqrt => square root"
+        + "\ninv => inverse"
+        + "\n+/- => change sign";
         
         print(helpMsg);
     }
